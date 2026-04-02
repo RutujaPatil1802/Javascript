e1fc1359939c4b2ffadc90ae3392bb5f3979f8ca
RutujaPatil1802 <143099244+RutujaPatil1802@users.noreply.github.com>
2026-04-02 17:28:07 +0000

    datatypes in javascript

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,18 @@
+//"use strict"; //treats all JS code as newer version, can skip this also, still it follows newer version, but a good practice
+//alert(3+3) //we are using nodejs here, not browser where we have js engine, so no popup shown
+console.log("addition of 3+3"); console.log(3+3) // this works but not a good practice, no readability of code
+ 
+let name="Rutuja" //string, double or single quotes
+let age=22 //number --range 2^^53 , after it bigInt is used
+let isEmployed=false //boolean
+//string
+//number
+//boolean
+//null--> standalone value, can be assigned intentionally, it is defined but it is empty for eg- temp. from an instrument is not recieved yet->then null preferred instead of 0-C or undefined
+//undefined-->shows value is not defined yet, value not assigned
+//symbol-->show uniqueness mostly used in react
+//object-->
+
+/* **typeof null is "object"
+   **typeof undefined is "undefined"
+*/
